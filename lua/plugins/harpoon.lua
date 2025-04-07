b1723b1daabd81685195918b3a8c0301c3ef9774
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-07 18:49:25 +0200

    re-add maps for 1-2-3-4 buffers

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -35,5 +35,9 @@ return {
     map('n', "<leader>a", function() harpoon:list():add() end)
     map("n", "<C-a>", function() toggle_telescope(harpoon:list()) end,
         { desc = "Open harpoon window" })
+    map("n", "<leader>1", function() harpoon:list():select(1) end)
+    map("n", "<leader>2", function() harpoon:list():select(2) end)
+    map("n", "<leader>3", function() harpoon:list():select(3) end)
+    map("n", "<leader>4", function() harpoon:list():select(4) end)
       end
 }
