cb17fbde0ccc2da9865e8d73b07dae896f706485
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-22 14:34:48 +0200

    add and modify some custom maps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,21 +4,37 @@ map('n', '<leader>e', ':Ex<cr>', { desc = 'Explorer' })
 map('n', '<leader>w', ':w<cr>', { desc = 'save file' })
 map('n', '<leader>q', ':q<cr>', { desc = 'save file' })
 map('n', ';', ':', { desc = 'swap : to ;' })
-map('n', '<C-i>', ':tabp<cr>', { desc = 'go to next tab' })
-map('n', '<C-o>', ':tabn<cr>', { desc = 'go to prev tab' })
+map('n', '<C-i>', ':tabn<cr>', { desc = 'go to next tab' })
+map('n', '<C-o>', ':tabp<cr>', { desc = 'go to prev tab' })
 map('n', '<C-g>', '<esc>', { desc = 'go to prev tab' })
 map('n', '<leader>T', ':tab new<cr>', { desc = 'New tab' })
 map('n', '<leader>t', ':tab ter<cr>', { desc = 'New tab with terminal' })
-map('n', '<leader>ra', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'replace the word everywhere in the file/buffer' })
+map('n', '<leader>ra',
+  ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'replace the word everywhere in the file/buffer' })
+
 map('n', 'Q', ':noh<cr>', { desc = 'Disable Q and cleans mini buffer from messages' })
 map('n', '<leader>V', ':vert terminal<cr>', { desc = 'Open vertical in vertical split' })
 map('n', '<leader>v', ':vsplit<cr>', { desc = 'Vertical split' })
 map('n', '<leader>db', ':1, .-bdelete<cr>', { desc = 'Delete all buffers except tabbed' })
 map('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 map('n', '<leader>cd', ':cd %:p:h<cr>')
-map('n', '<leader>ra', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'replace the word everywhere in the file/buffer' })
+map('n', '<leader>ra',
+  ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'replace the word everywhere in the file/buffer' })
+
 map('n', '<leader>db', ':1, .-bdelete<cr>', { desc = 'Delete all buffers except tabbed' })
 map('n', '<leader>d', '"_dd')
+map('n', '<leader>fd', '<cmd>Trouble diagnostics toggle<cr>')
+map('n', '<F4>', '<cmd>:cd %:p:h<cr>')
+
+-- dvorak
+--map('n', 'h', '<down>')
+--map('n', 'd', '<left>')
+--map('n', 't', '<up>')
+--map('n', 'n', '<right>')
+--map('i', '<C-d>', '<left>')
+--map('i', '<C-h>', '<down>')
+--map('i', '<C-t>', '<up>')
+--map('i', '<C-n>', '<right>')
 
 -- insert mode
 map('i', '<C-h>', '<left>', { desc = 'Move left in insert mode'})
