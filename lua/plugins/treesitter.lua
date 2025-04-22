bef215073c8202124dfac13b715767209c8689d9
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-22 14:39:25 +0200

    format and remove unused var

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs',
-  
+
   opts = {
     ensure_installed = {
       'bash', 'c', 'lua',
@@ -10,18 +10,18 @@ return {
       'query', 'vim', 'vimdoc',
       'go', 'zig'
     },
-    
+
     ignore_install = { 'text' },
-    
+
     auto_install = true,
-    
+
     highlight = {
       enable = true,
     },
 
     indent = { enable = true },
 
-    disable = function(lang, buf)
+    disable = function(buf)
       local max_filesize = 100 * 1024
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
